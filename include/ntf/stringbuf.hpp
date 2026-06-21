40eb356f81b7e6f51dc09371732ee98386d40c16
nesktf <nesktf@proton.me>
2026-06-21 16:06:09 -0300

    StringBuf

---
+++ include/ntf/stringbuf.hpp
@@ -0,0 +1,187 @@
+#ifndef NTF_BUFSTR_HPP_
+#define NTF_BUFSTR_HPP_
+
+#include <ntf/core.hpp>
+
+#include <iterator>
+
+namespace ntf {
+
+namespace impl {
+
+template<typename Char>
+constexpr NTF_INLINE void memcpy_char(Char* dst, const Char* src, size_t n) {
+  if (std::is_constant_evaluated()) {
+    for (size_t i = 0; i < n; ++i) {
+      dst[i] = src[i];
+    }
+  } else {
+    memcpy(dst, src, n * sizeof(Char));
+  }
+}
+
+template<typename Char>
+constexpr NTF_INLINE void memset_char(Char* dst, int val, size_t n) {
+  if (std::is_constant_evaluated()) {
+    for (size_t i = 0; i < n; ++i) {
+      dst[i] = static_cast<Char>(val);
+    }
+  } else {
+    memset(dst, val, n * sizeof(Char));
+  }
+}
+
+} // namespace impl
+
+template<size_t N, typename Char = char>
+class StringBuf {
+public:
+  static_assert(N > 0, "N can't be 0");
+  using size_type = size_t;
+  using char_type = Char;
+
+  static constexpr size_type BUFFER_SIZE = N;
+  static constexpr size_type MAX_STRING_SIZE = N - 1;
+
+  using iterator = Char*;
+  using const_iterator = const Char*;
+
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+private:
+  static constexpr size_type _cap_len(size_type size) noexcept {
+    return size > MAX_STRING_SIZE ? MAX_STRING_SIZE : size;
+  }
+
+public:
+  constexpr StringBuf(uninitialized_t, size_type size) noexcept : _size(_cap_len(size)) {}
+
+  constexpr StringBuf() noexcept : _size(0) { impl::memset_char(_data, 0x00, N); }
+
+  constexpr explicit StringBuf(size_type size) noexcept : _size(_cap_len(size)) {
+    impl::memset_char(_data, 0x00, N);
+  }
+
+  template<size_t M>
+  constexpr StringBuf(const Char (&str)[M]) noexcept : _size(M - 1) {
+    static_assert(M <= N, "Char array is bigger than string buffer");
+    impl::memset_char(_data, 0x00, N);
+    impl::memcpy_char(_data, str, M);
+  }
+
+  constexpr explicit StringBuf(const Char* str) noexcept : _size(_cap_len(strlen(str))) {
+    impl::memset_char(_data, 0x00, N);
+    impl::memcpy_char(_data, str, _size);
+  }
+
+  constexpr StringBuf(const Char* str, size_type size) noexcept : _size(_cap_len(size)) {
+    impl::memset_char(_data, 0x00, N);
+    impl::memcpy_char(_data, str, _size);
+  }
+
+public:
+  constexpr void resize(size_type size) noexcept {
+    size = _cap_len(size);
+    if (size == _size) {
+      return;
+    } else if (size < _size) {
+      impl::memset_char(_data + size, 0x00, N - size);
+    } else {
+      impl::memset_char(_data + _size, 0x00, size - _size);
+    }
+    _size = size;
+  }
+
+  constexpr void resize(size_type size, const Char& ch) noexcept {
+    size = _cap_len(size);
+    if (size == _size) {
+      return;
+    } else if (size < _size) {
+      impl::memset_char(_data + size, 0x00, N - size);
+    } else {
+      impl::memset_char(_data + _size, ch, size - _size);
+    }
+    _size = size;
+  }
+
+public:
+  constexpr Char& at(size_type i) {
+    NTF_THROW_IF(i >= size(),
+                 std::out_of_range("Index out of range in StringBuf, was " + std::to_string(i)));
+    return _data[i];
+  }
+
+  constexpr const Char& at(size_type i) const {
+    NTF_THROW_IF(i >= size(),
+                 std::out_of_range("Index out of range in StringBuf, was " + std::to_string(i)));
+    return _data[i];
+  }
+
+  constexpr Char& operator[](size_type i) {
+    NTF_ASSERT(i < _size, "Out of range in StringBuf");
+    return _data[i];
+  }
+
+  constexpr const Char& operator[](size_type i) const {
+    NTF_ASSERT(i < _size, "Out of range in StringBuf");
+    return _data[i];
+  }
+
+public:
+  constexpr Char* data() noexcept { return _data; }
+
+  constexpr const Char* data() const noexcept { return _data; }
+
+  constexpr const Char* c_str() const noexcept { return _data; }
+
+  constexpr size_t size() const noexcept { return _size; }
+
+  constexpr size_t capacity() const noexcept { return MAX_STRING_SIZE; }
+
+  constexpr bool empty() const noexcept { return size() == 0; }
+
+public:
+  constexpr iterator begin() noexcept { return data(); }
+
+  constexpr const_iterator begin() const noexcept { return data(); }
+
+  constexpr const_iterator cbegin() const noexcept { return data(); }
+
+  constexpr iterator end() noexcept { return data() + size(); }
+
+  constexpr const_iterator end() const noexcept { return data() + size(); }
+
+  constexpr const_iterator cend() const noexcept { return data() + size(); }
+
+  constexpr reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+
+  constexpr const_reverse_iterator rbegin() const noexcept {
+    return const_reverse_iterator(end());
+  }
+
+  constexpr const_reverse_iterator crbegin() const noexcept {
+    return const_reverse_iterator(end());
+  }
+
+  constexpr reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+
+  constexpr const_reverse_iterator rend() const noexcept {
+    return const_reverse_iterator(begin());
+  }
+
+  constexpr const_reverse_iterator crend() const noexcept {
+    return const_reverse_iterator(begin());
+  }
+
+private:
+  Char _data[N];
+  size_type _size;
+};
+
+template<size_t N, typename Char>
+StringBuf(const Char (&)[N]) -> StringBuf<N, Char>;
+
+} // namespace ntf
+
+#endif
---
+++ test/src/stringbuf.cpp
@@ -0,0 +1,214 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <ntf/stringbuf.hpp>
+
+static constexpr char char_array[] = "test_data";
+static constexpr wchar_t wchar_array[] = L"test_data";
+
+TEST_CASE("StringBuf construction", "[StringBuf]") {
+  SECTION("Default construction") {
+    ntf::StringBuf<32> str;
+    static_assert(decltype(str)::BUFFER_SIZE == 32);
+    static_assert(std::same_as<decltype(str)::char_type, char>);
+    REQUIRE(str.capacity() == 31);
+    REQUIRE(str.size() == 0);
+    REQUIRE(str.empty());
+    REQUIRE(std::strlen(str.c_str()) == 0);
+    REQUIRE(str[str.size()] == '\0');
+    for (size_t i = 0; i < decltype(str)::BUFFER_SIZE; ++i) {
+      REQUIRE(str[i] == '\0');
+    }
+  }
+
+  SECTION("Array of char construction") {
+    ntf::StringBuf str = char_array;
+    static_assert(std::same_as<decltype(str)::char_type, char>);
+    static_assert(decltype(str)::BUFFER_SIZE == sizeof(char_array));
+    REQUIRE(str.capacity() == sizeof(char_array) - 1);
+    REQUIRE(str.size() == sizeof(char_array) - 1);
+    REQUIRE(!str.empty());
+    REQUIRE(str[str.size()] == '\0');
+
+    static constexpr ntf::StringBuf cons_str = char_array;
+    static_assert(std::same_as<decltype(cons_str)::char_type, char>);
+    static_assert(decltype(cons_str)::BUFFER_SIZE == sizeof(char_array));
+    static_assert(cons_str.capacity() == sizeof(char_array) - 1);
+    static_assert(cons_str.size() == sizeof(char_array) - 1);
+    static_assert(!cons_str.empty());
+    static_assert(*(cons_str.data() + cons_str.size()) == '\0'); // operator[] is not constexpr
+  }
+
+  SECTION("Array of wchar construction") {
+    ntf::StringBuf str = wchar_array;
+    static_assert(std::same_as<decltype(str)::char_type, wchar_t>);
+    static_assert(decltype(str)::BUFFER_SIZE == sizeof(char_array));
+    REQUIRE(str.capacity() == sizeof(char_array) - 1);
+    REQUIRE(str.size() == sizeof(char_array) - 1);
+    REQUIRE(!str.empty());
+    REQUIRE(str[str.size()] == '\0');
+
+    static constexpr ntf::StringBuf cons_str = wchar_array;
+    static_assert(std::same_as<decltype(cons_str)::char_type, wchar_t>);
+    static_assert(decltype(cons_str)::BUFFER_SIZE == sizeof(char_array));
+    static_assert(cons_str.capacity() == sizeof(char_array) - 1);
+    static_assert(cons_str.size() == sizeof(char_array) - 1);
+    static_assert(!cons_str.empty());
+    static_assert(*(cons_str.data() + cons_str.size()) == '\0'); // operator[] is not constexpr
+  }
+
+  SECTION("C string construction") {
+    const char* c_string = char_array;
+    const size_t len = std::strlen(c_string);
+    ntf::StringBuf<sizeof(char_array)> str{c_string};
+    REQUIRE(str.capacity() == len);
+    REQUIRE(str.size() == len);
+    REQUIRE(!str.empty());
+    REQUIRE(str[str.size()] == '\0');
+    for (size_t i = 0; i < len; ++i) {
+      REQUIRE(c_string[i] == str[i]);
+    }
+  }
+
+  SECTION("Uninitialized construction") {
+    ntf::StringBuf<32> str1{ntf::uninitialized, 64};
+    REQUIRE(str1.capacity() == 31);
+    REQUIRE(str1.size() == 31);
+    REQUIRE(!str1.empty());
+
+    ntf::StringBuf<32> str2{ntf::uninitialized, 16};
+    REQUIRE(str2.capacity() == 31);
+    REQUIRE(str2.size() == 16);
+    REQUIRE(!str2.empty());
+  }
+
+  SECTION("Truncated construction") {
+    ntf::StringBuf<5> str{char_array, sizeof(char_array)};
+    REQUIRE(str.capacity() == 4);
+    REQUIRE(str.size() == 4);
+    REQUIRE(!str.empty());
+    REQUIRE(str[0] == 't');
+    REQUIRE(str[1] == 'e');
+    REQUIRE(str[2] == 's');
+    REQUIRE(str[3] == 't');
+    REQUIRE(str[4] == '\0');
+
+    static constexpr ntf::StringBuf<5> cons_str{char_array, sizeof(char_array)};
+    static_assert(cons_str.capacity() == 4);
+    static_assert(cons_str.size() == 4);
+    static_assert(!cons_str.empty());
+    static_assert(*(cons_str.data() + 0) == 't');
+    static_assert(*(cons_str.data() + 1) == 'e');
+    static_assert(*(cons_str.data() + 2) == 's');
+    static_assert(*(cons_str.data() + 3) == 't');
+    static_assert(*(cons_str.data() + 4) == '\0');
+  }
+
+  SECTION("Construction from smaller string") {
+    ntf::StringBuf<2 * sizeof(char_array)> str = char_array;
+    REQUIRE(str.capacity() == 2 * sizeof(char_array) - 1);
+    REQUIRE(str.size() == sizeof(char_array) - 1);
+    REQUIRE(!str.empty());
+    for (size_t i = 0; i < str.size(); ++i) {
+      REQUIRE(str[i] == char_array[i]);
+    }
+    REQUIRE(str[str.size()] == '\0');
+
+    // Everything after the string should be zeros
+    for (size_t i = str.size(); i < decltype(str)::BUFFER_SIZE; ++i) {
+      REQUIRE(str[i] == '\0');
+    }
+
+    static constexpr ntf::StringBuf<2 * sizeof(char_array)> cons_str = char_array;
+    static_assert(cons_str.capacity() == 2 * sizeof(char_array) - 1);
+    static_assert(cons_str.size() == sizeof(char_array) - 1);
+    static_assert(!cons_str.empty());
+    static_assert(*(cons_str.data() + 0) == 't');
+    static_assert(*(cons_str.data() + 1) == 'e');
+    static_assert(*(cons_str.data() + 2) == 's');
+    static_assert(*(cons_str.data() + 3) == 't');
+    static_assert(*(cons_str.data() + 4) == '_');
+    static_assert(*(cons_str.data() + 5) == 'd');
+    static_assert(*(cons_str.data() + 6) == 'a');
+    static_assert(*(cons_str.data() + 7) == 't');
+    static_assert(*(cons_str.data() + 8) == 'a');
+    static_assert(*(cons_str.data() + 9) == '\0');
+  }
+}
+
+TEST_CASE("StringBuf resize", "[StringBuf]") {
+  ntf::StringBuf str = char_array;
+  REQUIRE(str.size() == sizeof(char_array) - 1);
+  REQUIRE(!str.empty());
+
+  str.resize(4);
+  REQUIRE(str.size() == 4);
+  REQUIRE(!str.empty());
+  REQUIRE(str[str.size()] == '\0');
+
+  SECTION("Resize grow zero") {
+    str.resize(8);
+    REQUIRE(str.size() == 8);
+    REQUIRE(!str.empty());
+    REQUIRE(str[str.size()] == '\0');
+    for (size_t i = 4; i < 8; ++i) {
+      REQUIRE(str[i] == '\0');
+    }
+  }
+
+  SECTION("Resize grow fill") {
+    str.resize(8, 'a');
+    REQUIRE(str.size() == 8);
+    REQUIRE(!str.empty());
+    REQUIRE(str[str.size()] == '\0');
+    for (size_t i = 4; i < 8; ++i) {
+      REQUIRE(str[i] == 'a');
+    }
+  }
+}
+
+TEST_CASE("StringBuf checked access", "[StringBuf]") {
+  ntf::StringBuf<32> str;
+  REQUIRE_THROWS(str.at(64));
+  REQUIRE_THROWS(str.at(32));
+  REQUIRE_NOTHROW(str.at(31));
+}
+
+TEST_CASE("StringBuf iterator access", "[StringBuf]") {
+  ntf::StringBuf str = char_array;
+
+  SECTION("Forward iterator") {
+    size_t i = 0;
+    for (auto it = str.begin(); it != str.end(); ++it) {
+      REQUIRE(*it == char_array[i]);
+      ++i;
+    }
+    i = 0;
+    for (auto it = str.cbegin(); it != str.cend(); ++it) {
+      REQUIRE(*it == char_array[i]);
+      ++i;
+    }
+    i = 0;
+    for (auto& ch : str) {
+      REQUIRE(ch == char_array[i]);
+      ++i;
+    }
+    i = 0;
+    for (const auto& ch : str) {
+      REQUIRE(ch == char_array[i]);
+      ++i;
+    }
+  }
+
+  SECTION("Reverse iterator") {
+    size_t i = str.size() - 1;
+    for (auto it = str.rbegin(); it != str.rend(); ++it) {
+      REQUIRE(*it == char_array[i]);
+      --i;
+    }
+    i = 0;
+    for (auto it = str.crbegin(); it != str.crend(); ++it) {
+      REQUIRE(*it == char_array[i]);
+      --i;
+    }
+  }
+}
